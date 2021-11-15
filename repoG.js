5963429db26fda53e888f7b8d3fcc96d9a333192
Việt Lê <op04569@gmail.com>
2021-11-15 20:16:17 +0700

    Tao project

---
+++ repoG.js
@@ -0,0 +1,17 @@
+{
+	"version": "1.1.7",
+	"change": ["listban"],
+	"modules": {
+		"commands": {
+			"chuibot": "2.0.0",
+			"listban": "3.1.2",
+			"rank": "3.1.2",
+			"goibot": "1.1.0",
+			"alime": "1.1.2",
+			"allbox": "1.0.0",
+			"bans": "1.1.2",
+		},
+		"events": {
+		}
+	}
+}
